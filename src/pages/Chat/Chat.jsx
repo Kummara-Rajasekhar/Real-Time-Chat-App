a012685cf8a37dbda2b6be0d5ebd968021a5f13a
K Rajasekhar <kummararajasekhar17092004@gmail.com>
2025-03-13 16:58:25 +0530

    4 day commit

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Chat.css'
+
+import LeftSider from '../../components/LeftSider/LiftSider'
+import ChatBox from '../../components/ChatBoc/ChatBox'
+import RightSider from '../../components/RightSider/RightSider'
 const Chat = () => {
   return (
-    <div>
-      
+    <div className='chat'>
+      <div className="chat-container">
+        <LeftSider/>
+        <ChatBox/>
+        <RightSider/>
+      </div>
     </div>
   )
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 import assets from '../../assets/assets'
 const Login = () => {
+
+  const [currstate,setcurrstate]=useState("Sign Up")
+
   return (
     <div className='login'>
         <img src={assets.logo_big} alt="" className='logo' />
         <form action="" className='login-form'>
-            <h2>Sign Up</h2>
-            <input type="text" placeholder='username' className='form-input' required />
+            <h2>{currstate}</h2>
+
+            {currstate==="Sign Up" ?
+            <input type="text" placeholder='username' className='form-input' required /> 
+            : null
+          }
             <input type="email" placeholder='emial' className='form-input'  required />
             <input type="password" placeholder='passwird' className='form-input' required />
-            <button type='submit'> Sign Up</button>
+            <button type='submit'> {currstate==="Sign Up" ? "Create account" :"Login now "}</button>
             <div className='login-term'>
                 <input type="checkbox" />
                 <p>Agree to terms of user & privacy policy.</p>
             </div>
             <div className='login-forgot'>
-                <p className='login-toggle'>Already have an account <span>Click here</span></p>
+              {currstate==="Sign Up" ?
+                <p className='login-toggle'>Already have an account <span onClick={()=>setcurrstate("Login")}>Login here</span></p>
+                :
+                <p className='login-toggle'>Create an account <span onClick={()=>setcurrstate("Sign Up")}>Click here</span></p>
+              }
 
             </div>
         </form>
