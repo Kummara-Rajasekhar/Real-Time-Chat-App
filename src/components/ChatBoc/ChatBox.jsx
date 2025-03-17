85424305b5cff50f1c79e872cac57b7a0401cf2f
K Rajasekhar <kummararajasekhar17092004@gmail.com>
2025-03-17 18:16:05 +0530

    7 day commit

--- src/components/ChatBoc/ChatBox.jsx
+++ src/components/ChatBoc/ChatBox.jsx
@@ -134,8 +134,6 @@ const ChatBox = () => {
           ))
         }
       </div>
-      
-      
 
       <div className="chat-input">
         <input onChange={(e) => setinput(e.target.value)} value={input} type="text" placeholder='Send a message' />
